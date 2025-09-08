8d10a3b3501ad04879d8ac36c48e64c16c62ec94
Arif Can <arifcan1511@gmail.com>
2025-09-08 18:38:54 +0300

    Proje alpha version

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ParallaxSlider from "@/components/parallax-slider"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#0F0F10]">
+      <ParallaxSlider />
+    </main>
+  )
+}
